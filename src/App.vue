6dd7d3439a48b52210308bc954da54d0f12250fc
Janrelle24 <lubianojanrelle@gmail.com>
2023-10-09 14:16:28 +0800

    doCalculation function are working; backspace function still on process of fixing

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@
 
       <div class="buttons">
 
-        <Basic v-show="!toggle" v-bind:current="current" @addNumber="doStuff($event)"></Basic>
-        <Scientific v-show="toggle"  v-bind:current="current" @addNumber="doStuff($event)"></Scientific>
+        <Basic v-show="!toggle" v-bind:current="current" @addNumber="doCalculation($event)"></Basic>
+        <Scientific v-show="toggle"  v-bind:current="current" @addNumber="doCalculation($event)"></Scientific>
 
       </div>
     </div>
@@ -53,7 +53,219 @@ export default{
       }else{
         this.mode = 'Basic'
       }
-    }
+    },
+    doCalculation: function(data) {
+      let temp;
+      if(Number(data) || data ==0){
+        temp =data
+        if(this.current === 0 ) this.current = "";
+        this.current+= "" + temp;
+      }
+      if(!Number(data)) {
+        console.log(data);
+
+        switch(data) {
+          
+          case "/": {
+            this.divide();
+            break;
+          }
+          case "←": {
+            this.backspace();
+            break;
+          }
+          case "C": {
+            this.clear();
+            break;
+          }
+          case "*": {
+            this.multiply();
+            break;
+          }
+          case "-": {
+            this.minus();
+            break;
+          }
+          case "+": {
+            this.plus();
+            break;
+          }
+          case "x²": {
+            this.square();
+            break;
+          }
+          case "(": {
+            this.openbracket();
+            break;
+          }
+          case ")": {
+            this.closebracket();
+            break;
+          }
+          case "=": {
+            this.equals();
+            break;
+          }
+          case "±": {
+            this.plusMinus();
+            break;
+          }
+          case "%": {
+            this.percent();
+            break;
+          }
+          case ".": {
+            this.decimal();
+            break;
+          }
+          case "sin": {
+            this.sin();
+            break;
+          }
+          case "cos": {
+            this.cos();
+            break;
+          }
+          case "tan": {
+            this.tan();
+            break;
+          }
+          case "ln": {
+            this.ln();
+            break;
+          }
+          case "e": {
+            this.exp();
+            break;
+          }
+          case "∘": {
+            this.degrees();
+            break;
+          }
+          case "x!": {
+            this.factorial();
+            break;
+          }
+          case "rad": {
+            this.radians();
+            break;
+          }
+          case "√": {
+            this.squareRoot();
+            break;
+          }
+          case "x^": {
+            this.exponent();
+            break;
+          }
+          case "π": {
+            this.pi();
+            break;
+          }
+          case "log": {
+            this.log();
+            break;
+          }
+        }
+      }
+    },
+    divide: function(){
+      this.current += "/";
+    },
+    backspace: function(){
+      this.current = this.current.substring(0, this.current.length - 1);
+    },
+    multiply: function(){
+      this.current += "*";
+    },
+    clear: function() {
+      this.current = 0;
+    },
+    minus: function(){
+      this.current += "-";
+    },
+    plus: function(){
+      this.current += "+";
+    },
+    square: function(){
+      this.current = (this.current * this.current);
+    },
+    openbracket: function(){
+      this.current += "(";
+    },
+    closebracket: function(){
+      this.current += ")";
+    },
+    equals: function(){
+      if ((this.current).indexOf("^") > -1) {
+        var base = (this.current).slice(0, (this.current).indexOf("^"));
+        var exponent = (this.current).slice((this.current).indexOf("^") + 1);
+        this.current = eval("Math.pow(" + base + "," + exponent + ")");
+        
+      } else {
+      this.current = eval(this.current);
+      }
+    },
+    plusMinus: function() {
+      if ( (this.current !== 0) && this.current.charAt(0) === "-" )  {
+        this.current = this.current.slice(1);
+      } else {
+        this.current = "-" + this.current;
+      }
+    },
+    percent: function(){
+      this.current = this.current / 100 ;
+    },
+    decimal: function(){
+      this.current += ".";
+    },
+    sin: function(){
+      this.current = Math.sin(this.current);
+    },
+    cos: function(){
+      this.current = Math.cos(this.current);
+    },
+    tan: function(){
+      this.current = Math.tan(this.current);
+    },
+    ln: function(){
+      this.current = Math.log(this.current);
+    },
+    exp: function(){
+      this.current = Math.exp(this.current);
+    },
+    degrees: function(){
+      this.current = this.current * (180 / Math.PI);
+    },
+    factorial: function () {
+      var number = 1;
+      if (this.current === 0) {
+        this.current = "1";
+      } else if (this.current < 0) {
+        this.current = NaN;
+      } else {
+        var number = 1;
+        for (var i = this.current; i > 0; i--) {
+          number *=  i;
+        }
+        this.current = number;
+      }
+    },
+    radians: function (){
+      this.current = this.current * (Math.PI * 180);
+    },
+    squareRoot: function(){
+      this.current = Math.sqrt(this.current);
+    },
+    exponent: function () {
+      this.current += "^";
+    },
+    pi: function() {
+      this.current = (this.current * Math.PI);
+    },
+    log: function () {
+      this.current = Math.log10(this.current);
+    },
   }
 }
 </script>
